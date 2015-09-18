baf25f301fe472596a903f73156c68f186fce38a
Heshanr <h.r.randika@gmail.com>
2015-09-18 21:24:41 +0530

    goal bone

--- app/src/main/java/com/ampt/bluetooth/activities/DeviceScanActivity.java
+++ app/src/main/java/com/ampt/bluetooth/activities/DeviceScanActivity.java
@@ -244,7 +244,6 @@ public class DeviceScanActivity extends ListActivity {
                 view = mInflator.inflate(R.layout.listitem_device, null);
                 viewHolder = new ViewHolder();
                 viewHolder.deviceAddress = (TextView) view.findViewById(R.id.device_address);
-                viewHolder.deviceName = (TextView) view.findViewById(R.id.device_name);
                 view.setTag(viewHolder);
             } else {
                 viewHolder = (ViewHolder) view.getTag();
